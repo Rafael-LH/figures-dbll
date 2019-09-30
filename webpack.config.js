adbdb3afa793cd724d8e9be48ef891f64e677990
Rafael Lopez <rafael.iskoat@gmail.com>
2019-09-29 20:35:34 -0500

    config dev and production

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,81 @@
-const path = require("path");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src/index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/public/assets/"
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  devtool: "source-map",
-  devServer: {
-    contentBase: path.resolve(__dirname, "public/"),
-    publicPath: "/",
-    compress: true,
-    port: 9000
-  },
-  module: {
-    rules: [
-      {
-        exclude: /(node_modules)/,
-        test: /\.(js|jsx)$/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-            plugins: [
-              "@babel/plugin-transform-runtime",
-              "@babel/plugin-proposal-class-properties"
-            ]
-          }
-        }
-      },
-      {
-        test: /\.?scss$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true
-            }
-          },
+module.exports = (env) => {
+  const typeMode = env.NODE_ENV;
+  const isDev = typeMode === 'development';
+  const plugins = [];
 
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      }
-    ]
+  if (!isDev) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    );
+    plugins.push(
+      new CleanWebpackPlugin({
+        cleanAfterEveryBuildPatterns: ['dist'],
+      }),
+    );
   }
+  return {
+    mode: isDev,
+    entry: {
+      index: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      publicPath: '/public/assets/',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devtool: isDev && 'source-map',
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public/'),
+      publicPath: '/',
+      compress: true,
+      port: 9000,
+    },
+    module: {
+      rules: [
+        {
+          exclude: /(node_modules)/,
+          test: /\.(js|jsx)$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: [
+                '@babel/plugin-transform-runtime',
+                '@babel/plugin-proposal-class-properties',
+              ],
+              sourceMap: !!isDev,
+            },
+          },
+        },
+        {
+          test: /\.(sass|scss)$/,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: !!isDev,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: !!isDev,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins,
+  };
 };
